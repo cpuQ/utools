a2da50a70de16f4333fb44c411a5c660ec5f69e9
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-07-08 14:46:37 +0200

    Fix commands being serverside

--- src/main/java/net/lugo/utools/command/FullBrightCommand.java
+++ src/main/java/net/lugo/utools/command/FullBrightCommand.java
@@ -1,17 +1,17 @@
 package net.lugo.utools.command;
 
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.lugo.utools.features.FullBright;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.*;
 
 public class FullBrightCommand {
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess ignoredCommandRegistryAccess, CommandManager.RegistrationEnvironment ignoredRegistrationEnvironment) {
-        dispatcher.register(CommandManager.literal("fullbright")
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess ignoredCommandRegistryAccess) {
+        dispatcher.register(ClientCommandManager.literal("fullbright")
                 .executes(context -> FullBright.toggleGamma())
-                .then(CommandManager.argument("value", integer())
+                .then(ClientCommandManager.argument("value", integer())
                         .executes(context -> FullBright.setValue((double) getInteger(context, "value") / 100))
                 )
         );
